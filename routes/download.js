de03dcbc29595fc5e7d26c1b0a122723abf2e5b6
Ishank goel <ishankgoel811@gmail.com>
2021-04-10 17:53:44 +0530

    changes

---
+++ routes/download.js
@@ -0,0 +1,15 @@
+const router  = require("express").Router(); 
+
+const File = require('../models/file'); 
+router.get('/:uuid', async(req,res)=>{
+    const file = await  File.findOne({uuid:req.params.uuid}); 
+    if(!file){
+        return res.render('download', {error:'Linkhas been expired'}); 
+       
+    }
+     
+     const filePath = `${__dirname}/../${file.path}`; 
+     res.send(filePath); 
+     res.download(filePath); 
+})
+module.exports =router; 
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express") ; 
+const app = express() ;
+require('dotenv').config();  
+const port = process.env.PORT || 5000 ; 
+
+const path = require('path'); 
+
+ app.use(express.static('public')); 
+const connectDb = require("./config/db") ; 
+connectDb() ; 
+
+app.set('views', path.join(__dirname , '/views')); 
+
+app.set('view engine' , 'ejs');
+
+app.use(express.json());    
+
+app.use("/api/files" , require("./routes/file")); 
+app.use("/files", require("./routes/show")); 
+app.use('/files/download', require('./routes/download')); 
+app.listen(port ,()=>{
+console.log(`listening at port ${port}`) ; 
+})
